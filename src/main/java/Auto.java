402a3d698f64000044046c25fe1f8b2436582708
Oscar Andrés Úsuga Nanclares <oscarandresu@gmail.com>
2021-10-18 21:21:30 -0500

    Se agrega la clase Auto

---
+++ src/main/java/Auto.java
@@ -0,0 +1,27 @@
+public class Auto {
+  String modelo;
+  int precio;
+  Asiento[] asientos;
+  String marca;
+  Motor motor;
+  int registro;
+
+  static int cantidadCreados;
+
+  int cantidadAsientos() {
+    return asientos.length;
+  }
+
+  String verificarIntegridad() {
+    if (this.registro == this.motor.registro) {
+      for (Asiento asiento: asientos) {
+        if (this.registro != asiento.registro) {
+          return "Las piezas no son originales";
+        }
+      }
+      return "El auto es original";
+    } else {
+      return "Las piezas no son originales";
+    }
+  }
+}
