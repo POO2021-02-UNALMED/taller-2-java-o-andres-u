dc747a702c4aac002e1a2df3181bb6f2dd88569b
Oscar Andrés Úsuga Nanclares <oscarandresu@gmail.com>
2021-10-18 21:23:59 -0500

    Se agrega la clase Motor

---
+++ src/main/java/Motor.java
@@ -0,0 +1,14 @@
+public class Motor {
+  int numeroCilindros;
+  String tipo;
+  int registro;
+
+  void cambiarRegistro(int registro) {
+    this.registro = registro;
+  }
+
+  void asignarTipo(String tipo) {
+    if ("eléctrico".equalsIgnoreCase(tipo) || "gasolina".equalsIgnoreCase(tipo))
+      this.tipo = tipo;
+  }
+}
